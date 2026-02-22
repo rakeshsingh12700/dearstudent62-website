add60e3837ae8ed4a86b30daf9a1dd13a7376e2d
Rakesh Singh <rakesh12700@gmail.com>
2026-02-22 14:40:54 +0530

    aded content from google search console

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,10 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/favicon-ds.svg" type="image/svg+xml" />
         <meta name="theme-color" content="#F97316" />
+        <meta
+          name="google-site-verification"
+          content="BVG38LEFDyBC12HFwGRxXz386tU23-CRr_a1ULKykcc"
+        />
       </Head>
       <body>
         <Main />
